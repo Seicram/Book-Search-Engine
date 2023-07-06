36fe463dfe1c573262670adaf2a4e1f6a6ae8c0f
marcies smith <marcies.smith@gmail.com>
2023-07-06 08:29:12 -0400

    fixed queries.js & added npm run build

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,3 +20,21 @@ export const GET_ME = gql`
     }
   }
 `;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+  }
+`;
